b1c4f8da36c71f67af4a1fa8d810bfcfa0f45eba
paras081 <68942863+paras081@users.noreply.github.com>
2020-09-30 09:25:30 +0530

    Add files via upload

---
+++ css/assets/db/db2.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sample";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// sql to create table
+
+$sql = "CREATE TABLE lab1 (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+UserName VARCHAR(30) NOT NULL,
+password VARCHAR(30) NOT NULL,
+email VARCHAR(30) NOT NULL
+)";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "Table lab1 created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
